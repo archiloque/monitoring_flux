1ee5860ada5ba019d973b96d1dfaf90e832a7c5c
Julien Kirch <code@archiloque.net>
2015-03-05 17:14:38 +0100

    backend: code reuse

--- backend/src/main/java/com/octo/monitoring_flux/backend/ApplicationBase.java
+++ backend/src/main/java/com/octo/monitoring_flux/backend/ApplicationBase.java
@@ -31,17 +31,12 @@ public abstract class ApplicationBase {
     /**
      * Logger for this class.
      */
-	private static final Logger LOGGER = LoggerFactory.getLogger(Backend.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Backend.class);
 
     /**
      * Monitoring features.
      */
     private MonitoringMessenger monitoringMessenger;
-
-    /**
-     * Jackson Mapper.
-     */
-    private final ObjectReader mapReader = new ObjectMapper().reader(Map.class);
 
     /**
      * Value of the redis key to read from.
@@ -62,129 +57,95 @@ public abstract class ApplicationBase {
      * Connection redis.
      */
     private Jedis redisConnection;
-    
-    protected ApplicationBase() {
-    	loadConfiguration();
-    	initRedisConnection();
-    	initZeroMQConnection();
 
-    	// Start
+    protected ApplicationBase() {
+        loadConfiguration();
+        initRedisConnection();
+        initZeroMQConnection();
+
+        // Start
         while (true) {
-        	// Pop message from queue
+            // Pop message from queue
             List<String> bundledMessage = redisConnection.blpop(0, redisKey);
             LOGGER.info("Received a message");
-            
-            // Sending message to ZEROMQ
             processMessage(bundledMessage.get(1));
         }
     }
-    
+
     /**
      * Load configuration file (properties)
      */
     protected void loadConfiguration() {
-    	 try {
-             applicationProperties.load(getClass().getResourceAsStream("/backend.properties"));
-             LOGGER.info("Configuration file < backend.properties > is loaded");
-         } catch (IOException e) {
+        try {
+            applicationProperties.load(getClass().getResourceAsStream("/backend.properties"));
+            LOGGER.info("Configuration file < backend.properties > is loaded");
+        } catch (IOException e) {
             LOGGER.error("Cannot parse file backend.properties, check existence and rights", e);
             System.exit(-3);
-         }
+        }
     }
-    
+
     /**
      * Connect to redis
      */
     protected void initRedisConnection() {
-    	String redisHost = "localhost";
-    	int redisPort = Integer.parseInt(applicationProperties.getProperty("redis.port"));
-    	 LOGGER.info("Initializing Redis connection on <" + redisHost + ":" + redisPort + ">");
-    	try {
-	        redisKey = applicationProperties.getProperty("redis.key"); 
-    		redisConnection = new Jedis(redisHost, redisPort);
-    		LOGGER.debug("Try to ping Redis : " + redisConnection.ping());
-	        LOGGER.info("Redis connection ETABLISHED");
-    	} catch(RuntimeException re) {
-    		LOGGER.error("Cannot connect to REDIS check it's started and available on "+ redisHost + ":" + redisPort, re);
-    		System.exit(-1);
-    	}
+        String redisHost = "localhost";
+        int redisPort = Integer.parseInt(applicationProperties.getProperty("redis.port"));
+        LOGGER.info("Initializing Redis connection on <" + redisHost + ":" + redisPort + ">");
+        try {
+            redisKey = applicationProperties.getProperty("redis.key");
+            redisConnection = new Jedis(redisHost, redisPort);
+            LOGGER.debug("Try to ping Redis : " + redisConnection.ping());
+            LOGGER.info("Redis connection ETABLISHED");
+        } catch (RuntimeException re) {
+            LOGGER.error("Cannot connect to REDIS check it's started and available on " + redisHost + ":" + redisPort, re);
+            System.exit(-1);
+        }
     }
-    
+
     /**
      * Connect to zeroMQ and initialize sender
      */
     protected void initZeroMQConnection() {
-    	int zmqPort = Integer.parseInt(applicationProperties.getProperty("zeromq.port"));
-    	try {
-	    	String moduleType = applicationProperties.getProperty("app.name");
-	    	String moduleId	= moduleType + "." + ManagementFactory.getRuntimeMXBean().getName();
-	    	monitoringMessenger = new MonitoringMessenger(moduleType, moduleId, zmqPort);
-	    	LOGGER.info("ZeroMQ connection ETABLISHED");
-    	} catch(RuntimeException re) {
-    		LOGGER.error("Cannot connect to ZeroMQ check it's started and available on port " + zmqPort, re);
-    		System.exit(-2);
-    	}
+        int zmqPort = Integer.parseInt(applicationProperties.getProperty("zeromq.port"));
+        try {
+            String moduleType = applicationProperties.getProperty("app.name");
+            String moduleId = moduleType + "." + ManagementFactory.getRuntimeMXBean().getName();
+            monitoringMessenger = new MonitoringMessenger(moduleType, moduleId, zmqPort);
+            LOGGER.info("ZeroMQ connection ETABLISHED");
+        } catch (RuntimeException re) {
+            LOGGER.error("Cannot connect to ZeroMQ check it's started and available on port " + zmqPort, re);
+            System.exit(-2);
+        }
     }
-    
+
     /**
      * Process a message arrived in the queue.
      *
      * @param message the raw message content.
      */
-	private void processMessage(String message) {
+    private void processMessage(String message) {
         LOGGER.info(message);
-        Date receivedMessageTimestamp = getCurrentTimestamp();
-        String receivedMessageTimestampAsString = formatDateAsRfc339(receivedMessageTimestamp);
-        Map<String, Object> parsedMessage;
+        ParsedMessage parsedMessage;
         try {
-            parsedMessage = mapReader.readValue(message);
+            parsedMessage = new ParsedMessage(message);
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
             e.printStackTrace();
             return;
         }
-        Map messageHeader = (Map) parsedMessage.get("header");
-
-        final String correlationId = (messageHeader.containsKey(MonitoringMessagesKeys.MONITORING_MESSAGE_CORRELATION_ID)) ? MonitoringUtilities.createCorrelationId() : (String) messageHeader.get(MonitoringMessagesKeys.MONITORING_MESSAGE_CORRELATION_ID);
-
-        Map<String, ?> body = (Map<String, ?>) parsedMessage.get("body");
-
-        monitoringMessenger.sendMonitoringMessage(
-                correlationId,
-                redisKey,
-                "Received message",
-                receivedMessageTimestampAsString,
-                receivedMessageTimestampAsString,
-                null,
-                null,
-                body,
-                messageHeader,
-                null,
-                null
-        );
+        sendMonitoringMessage(parsedMessage, "Received message", parsedMessage.receivedMessageTimestampAsString, null, null, null, null);
         // Java8 yeah...
         executorService.submit(() -> {
             LOGGER.info("Begin processing");
             String beginProcessingTimestampAsString = getCurrentTimestampAsRfc339();
             Map<String, Object> moreInfo = new HashMap<>();
             moreInfo.put("begin_processing_timestamp", beginProcessingTimestampAsString);
-            monitoringMessenger.sendMonitoringMessage(
-                    correlationId,
-                    redisKey,
-                    "Start processing",
-                    beginProcessingTimestampAsString,
-                    receivedMessageTimestampAsString,
-                    null,
-                    null,
-                    body,
-                    messageHeader,
-                    null,
-                    null
-            );
+            sendMonitoringMessage(parsedMessage, "Start processing", beginProcessingTimestampAsString, null, null, null, null);
             Map<String, Object> result = new HashMap<>();
 
             try {
-                Map<?, ?> resultContent = processMessage(body);
+                Map<?, ?> resultContent = processMessage(parsedMessage.body);
                 result.put("Status", "OK");
                 result.put("Content", resultContent);
             } catch (Exception e) {
@@ -197,20 +158,15 @@ public abstract class ApplicationBase {
             LOGGER.info("End processing");
             Date endProcessingTimestamp = getCurrentTimestamp();
             String endProcessingTimestampAsString = formatDateAsRfc339(endProcessingTimestamp);
-
-            monitoringMessenger.sendMonitoringMessage(
-                    correlationId,
-                    redisKey,
+            Double elapsedTime = ((double) (endProcessingTimestamp.getTime() - parsedMessage.receivedMessageTimestamp.getTime())) / 1000;
+            sendMonitoringMessage(
+                    parsedMessage,
                     "End processing",
                     endProcessingTimestampAsString,
-                    receivedMessageTimestampAsString,
                     endProcessingTimestampAsString,
-                    ((double) (endProcessingTimestamp.getTime() - receivedMessageTimestamp.getTime())) / 1000,
-                    body,
-                    messageHeader,
+                    elapsedTime,
                     result,
-                    moreInfo
-            );
+                    moreInfo);
             return null;
         });
     }
@@ -223,5 +179,83 @@ public abstract class ApplicationBase {
      * @throws Exception when something goes wrong.
      */
     protected abstract Map<?, ?> processMessage(Map<String, ?> message) throws Exception;
+
+    /**
+     * Send a message to the monitoring
+     *
+     * @param parsedMessage  the parsed message.
+     * @param messageType    the message type.
+     * @param timestamp      the timestamp.
+     * @param endTimestamp   optional: the timestamp of the end of the current action
+     * @param elapsedTime    optional: the elapsed time of the current action in second
+     * @param result         optional: the result of current action
+     * @param initialContent optional: the initial content
+     */
+    private void sendMonitoringMessage(
+            ParsedMessage parsedMessage,
+            String messageType,
+            String timestamp,
+            String endTimestamp,
+            Double elapsedTime,
+            Object result,
+            Map<String, Object> initialContent) {
+        monitoringMessenger.sendMonitoringMessage(
+                parsedMessage.correlationId,
+                redisKey,
+                messageType,
+                timestamp,
+                parsedMessage.receivedMessageTimestampAsString,
+                endTimestamp,
+                elapsedTime,
+                parsedMessage.body,
+                parsedMessage.header,
+                result,
+                initialContent
+        );
+    }
+
+    /**
+     * The parsed message.
+     */
+    private static final class ParsedMessage {
+
+        /**
+         * Jackson Mapper.
+         */
+        private final ObjectReader mapReader = new ObjectMapper().reader(Map.class);
+
+        /**
+         * Timestamp when message is received.
+         */
+        private final Date receivedMessageTimestamp = getCurrentTimestamp();
+
+        /**
+         * Timestamp when message is received as a String.
+         */
+        private final String receivedMessageTimestampAsString = formatDateAsRfc339(receivedMessageTimestamp);
+
+        /**
+         * Message's correlation id.
+         */
+        private final String correlationId;
+
+        /**
+         * Message's header.
+         */
+        private final Map header;
+
+        /**
+         * Message's body.
+         */
+        private final Map<String, ?> body;
+
+        private ParsedMessage(String message) throws IOException {
+            Map<String, Object> parsedMessage = mapReader.readValue(message);
+            header = (Map) parsedMessage.get("header");
+            body = (Map<String, ?>) parsedMessage.get("body");
+            correlationId = (header.containsKey(MonitoringMessagesKeys.MONITORING_MESSAGE_CORRELATION_ID)) ? MonitoringUtilities.createCorrelationId() : (String) header.get(MonitoringMessagesKeys.MONITORING_MESSAGE_CORRELATION_ID);
+
+        }
+    }
 
 }
